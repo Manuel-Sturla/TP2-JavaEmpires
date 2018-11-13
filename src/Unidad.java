a54d19e8cec41b23f49a0a6d053cb8612376d088
Unknown <santiagolampropulos@gmail.com>
2018-11-13 02:21:10 -0300

    meti un par de movimiento de las unidades

--- src/Unidad.java
+++ src/Unidad.java
@@ -1,13 +1,7 @@
 public class Unidad implements ElementoDeMapa{
 
-    int posicionHorizontal = 0;
-    int posicionVertical = 0;
-
-    public void moverHaciaDerecha(Mapa mapa){
-        Unidad unidadCopia = new Unidad();
-        mapa.agregarElementoDeMapa(unidadCopia, posicionHorizontal + 1, posicionVertical);
-        mapa.borrarElementoDeMapa(posicionHorizontal, posicionVertical);
-    }
+    int posicionHorizontal;
+    int posicionVertical;
 
     @Override
     public void actualizarPosicion(int coordenadaHorizontal, int coordenadaVertical) {
@@ -15,11 +9,48 @@ public class Unidad implements ElementoDeMapa{
         posicionVertical = coordenadaVertical;
     }
 
-    /*
-    public void moverHaciaIzquierda(Mapa mapa){
-        Unidad unidadCopia = new Unidad;
-        mapa.agregarElementoDeMapa(unidadCopia,posicionHorizontal - 1, posicionVertical);
 
-    }*/
+    public void moverHaciaDerecha(Mapa mapa){
+        Unidad unidadCopia = new Unidad();
+        mapa.agregarElementoDeMapa(unidadCopia, posicionHorizontal + 1, posicionVertical);
+        mapa.borrarElementoDeMapa(posicionHorizontal, posicionVertical);
+    }
+    public void moverHaciaIzquierda(Mapa mapa){
+        Unidad unidadCopia = new Unidad();
+        mapa.agregarElementoDeMapa(unidadCopia,posicionHorizontal - 1, posicionVertical);
+        mapa.borrarElementoDeMapa(posicionHorizontal,posicionVertical);
+    }
+    public void moverHaciaArriba(Mapa mapa){
+        Unidad unidadCopia = new Unidad();
+        mapa.agregarElementoDeMapa(unidadCopia,posicionHorizontal, posicionVertical + 1);
+        mapa.borrarElementoDeMapa(posicionHorizontal,posicionVertical);
+    }
+    public void moverHaciaAbajo(Mapa mapa) {
+        Unidad unidadCopia = new Unidad();
+        mapa.agregarElementoDeMapa(unidadCopia,posicionHorizontal,posicionVertical -1);
+        mapa.borrarElementoDeMapa(posicionHorizontal,posicionVertical);
+    }
+
+    public void moverHaciaArribaDer(Mapa mapa){
+        Unidad copiaUniada = new Unidad();
+        mapa.agregarElementoDeMapa(copiaUniada,posicionHorizontal +1,posicionVertical+1);
+        mapa.borrarElementoDeMapa(posicionHorizontal,posicionVertical);
+    }
+    public void moverHaciaAbajoDer(Mapa mapa){
+        Unidad copiaUniada = new Unidad();
+        mapa.agregarElementoDeMapa(copiaUniada,posicionHorizontal +1,posicionVertical-1);
+        mapa.borrarElementoDeMapa(posicionHorizontal,posicionVertical);
+    }
+    public void moverHaciaArribaIzq(Mapa mapa){
+        Unidad copiaUniada = new Unidad();
+        mapa.agregarElementoDeMapa(copiaUniada,posicionHorizontal -1,posicionVertical+1);
+        mapa.borrarElementoDeMapa(posicionHorizontal,posicionVertical);
+    }
+    public void moverHaciaAbajoIzq(Mapa mapa){
+        Unidad copiaUniada = new Unidad();
+        mapa.agregarElementoDeMapa(copiaUniada,posicionHorizontal -1,posicionVertical-1);
+        mapa.borrarElementoDeMapa(posicionHorizontal,posicionVertical);
+    }
+
 
 }
--- test/UnidadTest.java
+++ test/UnidadTest.java
@@ -1,15 +1,84 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UnidadTest {
 
     @Test
-    void moverUnidadUnaPosicionTest() {
+    void moverUnidadUnaPosicionDerechaTest() {
         Unidad unidad = new Unidad();
         Mapa mapa = new Mapa();
         mapa.agregarElementoDeMapa(unidad, 0,0);
         unidad.moverHaciaDerecha(mapa);
         assertTrue(mapa.estaOcupadoElCasillero(1,0));
+    }
+
+    @Test
+    void moverUnidadPosicionIzqTest(){
+        Unidad unidad =new Unidad();
+        Mapa mapa = new Mapa();
+        mapa.agregarElementoDeMapa(unidad,1,0);
+        unidad.moverHaciaIzquierda(mapa);
+        assertTrue(mapa.estaOcupadoElCasillero(0,0));
+    }
+
+    @Test
+    void moverUnidadPosicionArribaTest(){
+        Unidad unidad =new Unidad();
+        Mapa mapa = new Mapa();
+        mapa.agregarElementoDeMapa(unidad,0,0);
+        unidad.moverHaciaArriba(mapa);
+        assertTrue(mapa.estaOcupadoElCasillero(0,1));
+    }
+
+    @Test
+    void moverUnidadPosicionAbajoTest(){
+        Unidad unidad =new Unidad();
+        Mapa mapa = new Mapa();
+        mapa.agregarElementoDeMapa(unidad,0,1);
+        unidad.moverHaciaAbajo(mapa);
+        assertTrue(mapa.estaOcupadoElCasillero(0,0));
+        assertFalse(mapa.estaOcupadoElCasillero(0,1));
+    }
+
+    @Test
+    void moverUnidadPosicionArribaDerTest(){
+        Unidad unidad =new Unidad();
+        Mapa mapa = new Mapa();
+        mapa.agregarElementoDeMapa(unidad,0,0);
+        unidad.moverHaciaArribaDer(mapa);
+        assertTrue(mapa.estaOcupadoElCasillero(1,1));
+        assertFalse(mapa.estaOcupadoElCasillero(0,0));
+    }
+
+    @Test
+    void moverUnidadPosicionAbajoDerTest(){
+        Unidad unidad =new Unidad();
+        Mapa mapa = new Mapa();
+        mapa.agregarElementoDeMapa(unidad,1,1);
+        unidad.moverHaciaAbajoDer(mapa);
+        assertTrue(mapa.estaOcupadoElCasillero(2,0));
+        assertFalse(mapa.estaOcupadoElCasillero(1,1));
+    }
+
+    @Test
+    void moverUnidadPosicionArribaIzqTest(){
+        Unidad unidad =new Unidad();
+        Mapa mapa = new Mapa();
+        mapa.agregarElementoDeMapa(unidad,1,1);
+        unidad.moverHaciaArribaIzq(mapa);
+        assertTrue(mapa.estaOcupadoElCasillero(0,2));
+        assertFalse(mapa.estaOcupadoElCasillero(1,1));
+    }
+
+    @Test
+    void moverUnidadPosicionAbajoIzqTest(){
+        Unidad unidad =new Unidad();
+        Mapa mapa = new Mapa();
+        mapa.agregarElementoDeMapa(unidad,1,1);
+        unidad.moverHaciaAbajoIzq(mapa);
+        assertTrue(mapa.estaOcupadoElCasillero(0,0  ));
+        assertFalse(mapa.estaOcupadoElCasillero(1,1));
     }
 }
