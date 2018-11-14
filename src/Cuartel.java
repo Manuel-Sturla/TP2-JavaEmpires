51a7dd42dd7829ecde64bdd2bc1b97fdc2345dee
maurogg96 <39010602+maurogg96@users.noreply.github.com>
2018-11-13 22:40:05 -0300

    Add files via upload

---
+++ src/Cuartel.java
@@ -0,0 +1,23 @@
+package tp1;
+
+public class Cuartel {
+	int vida = 250;
+	int costo = 50;
+	int duracion = 3;
+	
+	 Espadachin crear_espadachin(){
+			
+		Espadachin unidad = new Espadachin();
+		 
+		return unidad;
+		
+	}
+	 
+	 Arquero crear_arquero(){
+			
+		Arquero unidad = new Arquero();
+		 
+		return unidad;
+		
+	}
+}
---
+++ src/Plaza_central.java
@@ -0,0 +1,16 @@
+package tp1;
+
+public class Plaza_central {
+	int vida = 450;
+	int costo = 100;
+	int duracion = 3;
+	
+	 Aldeano crear_aldeano(){
+		
+		Aldeano unidad = new Aldeano();
+		 
+		return unidad;
+		
+	}
+
+}
