fdc572a52324d2ee858758a4b00942b31357191a
Unknown <santiagolampropulos@gmail.com>
2018-11-20 00:56:20 -0300

    Herencia 1.2 y cambios en PosicionEdificios mas detalles describcion
    ahora PosicioonEdificio recibe la construccion la posicion de inicio de construccion y se verifica si esta a rango del aldeano. si hay dudas preguntarme

--- src/Ubicables/Edificios.java
+++ src/Ubicables/Edificios.java
@@ -1,16 +1,39 @@
 package Ubicables;
 
+import Exceptions.PosicionFueraDeRangoException;
 import Tablero.Posicion;
 import Tablero.PosicionEdificio;
+import Turnos.Desocupado;
 import Turnos.Estado;
 
 public abstract class Edificios implements Ubicable {
 
-    int vida,vidaActual;
-    PosicionEdificio posicion;
-    Estado estado;
+    protected int vida,vidaActual;
+    protected PosicionEdificio posicion;
+    protected Estado estado;
 
-    public Edificios(Posicion posicionAldeano){
-
+    public Edificios(Posicion posicionInicioConstruccion,int largo,int vidaRecibida) throws PosicionFueraDeRangoException {
+        posicion = new PosicionEdificio(posicionInicioConstruccion,largo);
+        posicion.ocuparPosiciones(this);
+        int vida =vidaRecibida;
+        int vidaActual = vidaRecibida;
+        Estado estado = new Desocupado();
     }
+
+    public void desocuparUnTurno() {
+        estado = estado.desocuparUnTurno();
+    }
+    public boolean estaOcupada() {
+        return estado.estaOcupado();
+    }
+
+    @Override
+    public boolean estaMuerto() {
+        return vidaActual<1;
+    }
+    @Override
+    public void quitarVida(int dañoRecibido) {
+        vidaActual -= dañoRecibido;
+    }
+
 }
--- src/Ubicables/PlazaCentral.java
+++ src/Ubicables/PlazaCentral.java
@@ -1,33 +1,15 @@
 package Ubicables;
 
-import Exceptions.NoSePudoConstruirException;
 import Exceptions.PosicionFueraDeRangoException;
 import Exceptions.UbicableEstaOcupadoException;
 import Tablero.Posicion;
-import Tablero.PosicionEdificio;
-import Turnos.Estado;
-import Turnos.Ocupado;
-import Ubicables.Ubicable;
-
-public class PlazaCentral implements Ubicable {
-    PosicionEdificio posicion;
-    Estado estado;
-    private int vida= 450;
-    private int vidaActual = vida;
 
 
-    public PlazaCentral(Posicion posicionAldeano) throws PosicionFueraDeRangoException, NoSePudoConstruirException {
-        posicion = new PosicionEdificio(posicionAldeano, 4);
-        posicion.ocuparPosiciones(this);
-        estado = new Ocupado(3);
-    }
+public class PlazaCentral extends Edificios {
 
-    public void desocuparUnTurno() {
-        estado = estado.desocuparUnTurno();
-    }
 
-    public boolean estaOcupada() {
-        return estado.estaOcupado();
+    public PlazaCentral(Posicion posicionAldeano) throws PosicionFueraDeRangoException{
+        super(posicionAldeano,2,450);
     }
 
     public Aldeano crearAldeano() throws UbicableEstaOcupadoException, PosicionFueraDeRangoException {
@@ -39,13 +21,4 @@ public class PlazaCentral implements Ubicable {
     }
 
 
-    @Override
-    public boolean estaMuerto() {
-        return vidaActual<1;
-    }
-
-    @Override
-    public void quitarVida(int dañoRecibido) {
-        vidaActual -= dañoRecibido;
-    }
 }
