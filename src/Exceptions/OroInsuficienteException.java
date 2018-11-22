3b5566575b84c5e6cad93459b3b3e0aefa19257f
maurogg96 <39010602+maurogg96@users.noreply.github.com>
2018-11-21 23:38:52 -0300

    Excepciones para el manejador

---
+++ src/Exceptions/OroInsuficienteException.java
@@ -0,0 +1,4 @@
+package Exceptions;
+
+public class OroInsuficienteException extends Exception {
+}
---
+++ src/Exceptions/UnidadesMaximasException.java
@@ -0,0 +1,4 @@
+package Exceptions;
+
+public class UnidadesMaximasException extends Exception{
+}
