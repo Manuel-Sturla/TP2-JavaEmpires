ee0c1d9dcbef0ccff99383425c43cd9b5113454a
maurogg96 <39010602+maurogg96@users.noreply.github.com>
2018-11-30 19:12:55 -0300

    Agregadas algunas pruebas de movimiento diagonal

--- test/PruebasUbicables/PruebasUnidades/ArmaDeAsedioTest.java
+++ test/PruebasUbicables/PruebasUnidades/ArmaDeAsedioTest.java
@@ -54,6 +54,26 @@ public class ArmaDeAsedioTest {
         Posicion posicionLlegada = new Posicion(mapa, 4,3);
         assertTrue(mapa.celdaEstaOcupada(posicionLlegada));
     }
+    
+    @Test
+    void moverArmaArribaADerechaCuandoNoEstaMontadaALaMueve() throws UbicableEstaOcupadoException, PosicionInvalidaException {
+        Mapa mapa = new Mapa(10,10);
+        Posicion posicion = new Posicion(mapa, 3,3);
+        ArmaDeAsedio armaDeAsedio = new ArmaDeAsedio(posicion);
+        armaDeAsedio.moverArribaDer();
+        Posicion posicionLlegada = new Posicion(mapa, 4,4);
+        assertTrue(mapa.celdaEstaOcupada(posicionLlegada));
+    }
+
+    @Test
+    void moverArmaAbajoAIzquierdaCuandoNoEstaMontadaALaMueve() throws UbicableEstaOcupadoException, PosicionInvalidaException {
+        Mapa mapa = new Mapa(10,10);
+        Posicion posicion = new Posicion(mapa, 3,3);
+        ArmaDeAsedio armaDeAsedio = new ArmaDeAsedio(posicion);
+        armaDeAsedio.moverAbajoIzq();
+        Posicion posicionLlegada = new Posicion(mapa, 2,2);
+        assertTrue(mapa.celdaEstaOcupada(posicionLlegada));
+    }
 
     @Test
     void moverArmaCuandoEstaMontadaNoSeMueve() throws PosicionInvalidaException {
