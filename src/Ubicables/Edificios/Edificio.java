8900f0ce09267ddb5432630868ad49d7e07bd778
maurogg96 <39010602+maurogg96@users.noreply.github.com>
2018-12-03 16:28:47 -0300

    agregado codigo en caso de muerte de un edificio

--- src/Ubicables/Edificios/Edificio.java
+++ src/Ubicables/Edificios/Edificio.java
@@ -47,7 +47,43 @@ public abstract class Edificio extends Ubicable {
         return posicion;
     }
 
-    public void recibirDanio(Espadachin espadachin){ vida -= 15; }
-    public void recibirDanio(Arquero arquero){ vida -= 10;}
-    public void recibirDanio(ArmaDeAsedio armaDeAsedio){ vida -= 75;}
+    public void recibirDanio(Espadachin espadachin) throws PosicionInvalidaException {
+        //vida -= 15;
+        if(vidaMax == 1000)
+            super.recibirDanio(15,true);
+        super.recibirDanio(15,false);
+        if (vida < 1)
+            morir();
+    }
+
+    public void recibirDanio(Arquero arquero) throws PosicionInvalidaException {
+        //vida -= 10;
+        if (vidaMax == 1000)
+            super.recibirDanio(10,true);
+        super.recibirDanio(10,false);
+        if(vida < 1)
+            morir();
+    }
+    public void recibirDanio(ArmaDeAsedio armaDeAsedio) throws PosicionInvalidaException {
+        //vida -= 75;
+        if(vidaMax == 1000)
+            super.recibirDanio(75,true);
+        super.recibirDanio(75,false);
+        if(vida < 1)
+            morir();
+    }
+
+    public void recibirDanio(Castillo castillo) throws PosicionInvalidaException {
+        if (vidaMax == 1000)
+            super.recibirDanio(20,true);
+        super.recibirDanio(20,false);
+        if(vida < 1)
+            morir();
+    }
+
+    public void morir() throws PosicionInvalidaException {
+
+        posicion.desocuparPosiciones();
+
+    }
 }
